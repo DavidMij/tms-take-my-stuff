3e9ddf878b199157d80961c438f418f6ac8dff71
david mijan <david.mijan@zeekit.me>
2023-06-10 17:20:54 +0300

    add payment

--- src/libs/functions/index.js
+++ src/libs/functions/index.js
@@ -1,5 +1,6 @@
 import { parse } from "cookie";
 import * as jose from "jose";
+import { loadStripe } from "@stripe/stripe-js";
 
 export const extractUserIdFromCookies = async (cookie) => {
   const { tmsToken } = parse(cookie || "");
@@ -10,3 +11,25 @@ export const extractUserIdFromCookies = async (cookie) => {
 
   return payload.userId;
 };
+
+
+export async function checkout({lineItems}){
+  let stripePromise = null
+
+  const getStripe = () => {
+    if(!stripePromise) {
+      stripePromise = loadStripe(process.env.NEXT_PUBLIC_API_KEY)
+    }
+    return stripePromise
+  }
+
+  const stripe = await getStripe()
+
+  await stripe.redirectToCheckout({
+    mode: 'payment',
+    lineItems,
+    successUrl: `${window.location.href}?success=true&session_id={CHECKOUT_SESSION_ID}`,
+    cancelUrl: window.location.origin
+  })
+
+}
--- src/pages/pdp/[id].js
+++ src/pages/pdp/[id].js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import http from "../../axios";
 import {Button, Checkbox, Dialog, FormControlLabel, Grid, styled, Typography} from "@mui/material";
 import {useRouter} from "next/router";
+import {checkout} from "../../libs/functions";
 
 const StyledDialog = styled(Dialog)`
   & .MuiPaper-root {
@@ -13,116 +14,127 @@ const StyledDialog = styled(Dialog)`
 `;
 
 export const PDP = () => {
-    const router = useRouter();
-    const id = router?.query?.id;
+        const router = useRouter();
+        const id = router?.query?.id;
 
-    const [property, setProperty] = useState([]);
-    const [isDialogOpen, setIsDialogOpen] = React.useState(false);
-    const [isInsuranceIncluded, setIsInsuranceIncluded] = React.useState(false);
+        const [property, setProperty] = useState([]);
+        const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+        const [isInsuranceIncluded, setIsInsuranceIncluded] = React.useState(false);
 
-    useEffect(() => {
-        if (id) {
-            http.get(`/api/get-property?id=${id}`).then((res) => {
-                setProperty([res.data.property]);
+        useEffect(() => {
+            if (id) {
+                http.get(`/api/get-property?id=${id}`).then((res) => {
+                    setProperty([res.data.property]);
+                });
+            }
+        }, [id]);
+
+        const checkApproval = () => {
+            setIsDialogOpen(true);
+        }
+
+        const handleRentNowCTA = async (property) => {
+
+            http
+                .post("/api/rent-property", {
+                    _id: property._id,
+                    propertyOwner: property.userId,
+                    propertyName: property.propertyName,
+                })
+                .then((res) => {
+                    if (res.status === 200) {
+                        property.isRented = true
+                        setProperty([property]);
+                        setIsDialogOpen(false);
+                    }
+                });
+            await checkout({
+                lineItems: [
+                    {
+                        price: "price_1NHQ2vEG3PfrDCC7rC6x1VOX",
+                        quantity: 1
+                    }]
             });
         }
-    }, [id]);
-
-    const checkApproval = () => {
-        setIsDialogOpen(true);
-    }
-
-    const handleRentNowCTA = (property) => {
-        http
-            .post("/api/rent-property", {
-                _id: property._id,
-                propertyOwner: property.userId,
-                propertyName: property.propertyName,
-            })
-            .then((res) => {
-                if (res.status === 200) {
-                    property.isRented = true
-                    setProperty([property]);
-                    setIsDialogOpen(false);
-                }
-            });
-    };
 
 
-    return (
-        <Grid container sx={{mt: 5, p: "0 55px"}}>
-            {
-                property?.map(
-                    ({
-                         propertyName,
-                         propertyAddress,
-                         propertyDescription,
-                         isRented,
-                         price,
-                         image,
-                         endDate,
-                         startDate,
-                         _id,
-                         category,
-                         userId,
-                     }) => {
-                        return (
-                            <Grid item key={_id}>
-                                <img style={{borderRadius: "3vmin", height: "200px"}} src={image}></img>
-                                <Typography variant="h2" sx={{mb: 2, fontWeight: "bold"}}>
-                                    {propertyName}
-                                </Typography>
-                                <Typography>Address: {propertyAddress}</Typography>
-                                <Typography>Category: {category}</Typography>
-                                <Typography>Price: {price}</Typography>
-                                <Typography>Start Date: {new Date(startDate).toDateString()}</Typography>
-                                <Typography>End Date: { new Date(endDate).toDateString()}</Typography>
-                                <Typography>Description: {propertyDescription}</Typography>
-                                <Grid item>
-                                    <Button
-                                        disabled={isRented}
-                                        variant="contained"
-                                        size="large"
-                                        onClick={() => {
-                                            checkApproval();
-
-                                        }
-                                        }
-                                        sx={{mt: 5}}
-                                    >
-                                        {isRented ? "Rented" : "Rent it now!"}
-                                    </Button>
-                                </Grid>
-
-                                <StyledDialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
-                                    <Typography variant="h2" align="center"
-                                                sx={{pt: 5, fontWeight: 'bold', maxWidth: '550px', m: '0 auto'}}>Are you
-                                        sure you want to rent it?</Typography>
-
-                                    <Grid container justifyContent="center" sx={{mt: 3, mb: 3}}>
-                                        <FormControlLabel
-                                            control={<Checkbox checked={isInsuranceIncluded}
-                                                               onChange={(e) => setIsInsuranceIncluded(e.target.checked)}/>}
-                                            label="Include property insurance"/>
-                                    </Grid>
-
-
-                                    <Grid container justifyContent="center">
+        return (
+            <Grid container sx={{mt: 5, p: "0 55px"}}>
+                {
+                    property?.map(
+                        ({
+                             propertyName,
+                             propertyAddress,
+                             propertyDescription,
+                             isRented,
+                             price,
+                             image,
+                             endDate,
+                             startDate,
+                             _id,
+                             category,
+                             userId,
+                         }) => {
+                            return (
+                                <Grid item key={_id}>
+                                    <img style={{borderRadius: "3vmin", height: "200px"}} src={image}></img>
+                                    <Typography variant="h2" sx={{mb: 2, fontWeight: "bold"}}>
+                                        {propertyName}
+                                    </Typography>
+                                    <Typography>Address: {propertyAddress}</Typography>
+                                    <Typography>Category: {category}</Typography>
+                                    <Typography>Price: {price}</Typography>
+                                    <Typography>Start Date: {new Date(startDate).toDateString()}</Typography>
+                                    <Typography>End Date: {new Date(endDate).toDateString()}</Typography>
+                                    <Typography>Description: {propertyDescription}</Typography>
+                                    <Grid item>
                                         <Button
-                                            sx={{width: '250px', mr: 4, height: '50px', fontSize: '18px'}}
+                                            disabled={isRented}
                                             variant="contained"
-                                            onClick={() => handleRentNowCTA(property[0])}>Yes</Button>
-                                        <Button variant="outlined"
-                                                sx={{width: '250px', height: '50px', fontSize: '18px'}}
-                                                onClick={() => setIsDialogOpen(false)}>No, Maybe next time</Button>
+                                            size="large"
+                                            onClick={() => {
+                                                checkApproval();
+
+                                            }
+                                            }
+                                            sx={{mt: 5}}
+                                        >
+                                            {isRented ? "Rented" : "Rent it now!"}
+                                        </Button>
                                     </Grid>
-                                </StyledDialog>
-                            </Grid>
-                        );
-                    }
-                )}
-        </Grid>
-    );
-};
+
+                                    <StyledDialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+                                        <Typography variant="h2" align="center"
+                                                    sx={{pt: 5, fontWeight: 'bold', maxWidth: '550px', m: '0 auto'}}>Are you
+                                            sure you want to rent it?</Typography>
+
+                                        <Grid container justifyContent="center" sx={{mt: 3, mb: 3}}>
+                                            <FormControlLabel
+                                                control={<Checkbox checked={isInsuranceIncluded}
+                                                                   onChange={(e) => setIsInsuranceIncluded(e.target.checked)}/>}
+                                                label="Include property insurance"/>
+                                        </Grid>
+
+
+                                        <Grid container justifyContent="center">
+                                            <Button
+                                                sx={{width: '250px', mr: 4, height: '50px', fontSize: '18px'}}
+                                                variant="contained"
+                                                onClick={() => {
+                                                    handleRentNowCTA(property[0])
+                                                }}>Yes</Button>
+                                            <Button variant="outlined"
+                                                    sx={{width: '250px', height: '50px', fontSize: '18px'}}
+                                                    onClick={() => setIsDialogOpen(false)}>No, Maybe next time</Button>
+                                        </Grid>
+                                    </StyledDialog>
+                                </Grid>
+                            );
+                        }
+                    )}
+            </Grid>
+        );
+    }
+;
 
 export default PDP;
